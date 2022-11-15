15dca291f28d77712d1750285470356c000715f9
Juan-P1224 <juanp.cardonag@uqvirtual.edu.co>
2022-11-14 19:41:42 -0500

    Seleccionar del teatro

--- web/src/main/java/co/edu/uniquindio/unicine/bean/TeatroBean.java
+++ web/src/main/java/co/edu/uniquindio/unicine/bean/TeatroBean.java
@@ -1,15 +1,22 @@
 package co.edu.uniquindio.unicine.bean;
 
 import co.edu.uniquindio.unicine.entidades.AdministradorTeatro;
+import co.edu.uniquindio.unicine.entidades.Ciudad;
 import co.edu.uniquindio.unicine.entidades.Teatro;
+import co.edu.uniquindio.unicine.servicios.AdminServicio;
 import co.edu.uniquindio.unicine.servicios.AdminTeatroServicio;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @ViewScoped
@@ -17,15 +24,65 @@ public class TeatroBean implements Serializable {
 
     @Getter @Setter
     private Teatro teatro;
-
-    @Getter @Setter
-    private  AdministradorTeatro administradorTeatro;
-
     @Autowired
     private AdminTeatroServicio adminTeatroServicio;
-
-    public void crearTeatro(){
-
+    @Autowired
+    private AdminServicio adminServicio;
+    @Getter @Setter
+    private List<Teatro> teatros;
+    private boolean editar;
+    @Getter @Setter
+    private List<Ciudad> ciudades;
+    @Getter @Setter
+    private List<Teatro> teatrosSeleccionados;
+    @PostConstruct
+    public void inicializar(){
+        teatro = new Teatro();
+        editar = false;
+        ciudades = adminServicio.listarCiudades();
+        teatros = adminTeatroServicio.listarTeatros();
+        teatrosSeleccionados = new ArrayList<>();
     }
 
+    public void crearTeatro(){
+        try {
+            //Esto debe borrar cuando se implemente la sesion
+            AdministradorTeatro administradorTeatro = adminServicio.obtenerAdminTeatro(1);
+            teatro.setAdministrador(administradorTeatro);
+            //Nos ayuda para actualizar las listas
+            Teatro registro = adminTeatroServicio.crearTeatro(teatro);
+            teatros.add(registro);
+            teatro = new Teatro();
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", "Teatro creado correctamente");
+            FacesContext.getCurrentInstance().addMessage("mensaje_bean", fm);
+        }catch (Exception e){
+            FacesMessage fm  = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", e.getMessage());
+            FacesContext.getCurrentInstance().addMessage("mensaje_bean", fm);
+        }
+    }
+
+    public void eliminarTeatro(){
+        try {
+            for (Teatro t: teatrosSeleccionados ){
+                adminTeatroServicio.eliminarTeatro(t.getCodigo());
+                teatros.remove(t);
+            }
+            teatrosSeleccionados.clear();
+        }catch (Exception e){
+            FacesMessage fm  = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", e.getMessage());
+            FacesContext.getCurrentInstance().addMessage("mensaje_bean", fm);
+        }
+    }
+
+    public String getMensajeBorrar(){
+        if(teatrosSeleccionados.size()==0){
+            return "Borrar";
+        }else{
+            return teatrosSeleccionados.size() == 1 ? "Borrar 1 elemento": "Borrar "+teatrosSeleccionados.size()+" elementos";
+        }
+    }
+    public void seleccionarTeatro(Teatro teatroSeleccionado){
+        this.teatro = teatroSeleccionado;
+        editar = true;
+    }
 }
---
+++ web/src/main/java/co/edu/uniquindio/unicine/converter/CiudadConverter.java
@@ -0,0 +1,36 @@
+package co.edu.uniquindio.unicine.converter;
+
+import co.edu.uniquindio.unicine.entidades.Ciudad;
+import co.edu.uniquindio.unicine.servicios.AdminServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+@Component
+public class CiudadConverter implements Converter<Ciudad> {
+
+    @Autowired
+    private AdminServicio adminServicio;
+
+    @Override
+    public Ciudad getAsObject(FacesContext context, UIComponent component, String value) {
+
+        Ciudad ciudad;
+
+        try {
+            ciudad = adminServicio.obtenerCiudad(Integer.parseInt(value));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ciudad;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Ciudad value) {
+        if(value != null){ return ""+value.getCodigo();}
+        return "";
+    }
+}
